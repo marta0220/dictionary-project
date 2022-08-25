20767257ca81e3531bbca5517638dc50847abb85
marta0220 <109087240+marta0220@users.noreply.github.com>
2022-08-25 14:03:55 +0300

    Made search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">Dictionary content</div>;
+  let [keyword, setKeyword] = useState(null);
+  let [data, setData] = useState({});
+  function handleResponse(response) {
+    setData(response.data[0]);
+  }
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleChangeOfKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
+      <h5>What word would you like to look up?</h5>
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Search"
+          onInput={handleChangeOfKeyword}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="word">{data.word}</div>
+      <div className="text-muted text-start">{data.phonetic}</div>
+    </div>
+  );
 }
